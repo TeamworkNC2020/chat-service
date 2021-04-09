fa34dd6e2957ce4c863b6d0fadd8311e5998ca43
weldnor <kolesov.anton.s@gmail.com>
2021-04-10 00:26:00 +0300

    WIP add allowed origins

--- src/main/java/com/moviesandchill/chatservice/config/WebSocketConfig.java
+++ src/main/java/com/moviesandchill/chatservice/config/WebSocketConfig.java
@@ -11,8 +11,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // FIXME
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:4200", "http://localhost:4200")
+                .setAllowedOrigins("http://localhost:4200", "http://localhost:8080", "http://localhost:8081")
                 .withSockJS();
     }
 
