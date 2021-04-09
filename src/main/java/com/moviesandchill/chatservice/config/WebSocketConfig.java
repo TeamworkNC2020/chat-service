681131706135e14c7b06e9576d8943e89774bfd5
weldnor <kolesov.anton.s@gmail.com>
2021-04-09 13:42:45 +0300

    Add simple sessions topic and page for test it

--- src/main/java/com/moviesandchill/chatservice/config/WebSocketConfig.java
+++ src/main/java/com/moviesandchill/chatservice/config/WebSocketConfig.java
@@ -12,13 +12,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:4200")
+                .setAllowedOrigins("http://localhost:4200", "http://localhost:4200")
                 .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        //registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/message");
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/sessions");
     }
 }
